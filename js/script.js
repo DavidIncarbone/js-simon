d17add3ca6721c12976395286ccef63c305178fc
DavidIncarbone <david.incarbone@gmail.com>
2024-11-11 13:57:07 +0100

    Get elements from index.html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 "use strict"
 console.clear();
 
+// Get Elements
+
+let countDown = document.getElementById("countdown");
+console.log(countDown);
+const numberList = document.getElementById("numbers-list");
+console.log(numberList);
+const answersForm = document.getElementById("answers-form");
+console.log(answersForm);
+const message = document.getElementById("message");
+console.log(message);
+
+
+
+
 
 
 
