b28e7d9ef3f69dc5061e008d3b7a848debf6a1c4
DavidIncarbone <david.incarbone@gmail.com>
2024-11-12 07:07:10 +0100

    fixed final message

--- js/script.js
+++ js/script.js
@@ -12,11 +12,13 @@ let numbersList = document.getElementById("numbers-list");
 const answersForm = document.getElementById("answers-form");
 // console.log(answersForm);
 const message = document.getElementById("message");
+message.classList.add("d-none");
 // console.log(message);
 const container = document.querySelector("body>div");
 // console.log(container);
 const inputs = document.querySelectorAll("input");
 console.log(inputs);
+
 
 
 
@@ -80,7 +82,10 @@ console.log(replayBtn);
 
 
 answersForm.addEventListener("submit", function (event) {
+
     event.preventDefault();
+
+
     answersForm.classList.add("d-none");
     replayBtn.classList.add("btn", "btn-primary", "mt-2", "d-block", "mx-auto");
     container.appendChild(replayBtn);
@@ -93,27 +98,30 @@ answersForm.addEventListener("submit", function (event) {
 
         const field = parseInt(inputs[i].value);
 
+
         userNumbers.push(field);
     }
-    console.log(userNumbers);
 
+    console.log(userNumbers);
 
     // check guess numbers
 
     const guessedNumbers = [];
     for (let i = 0; i <= userNumbers.length; i++) {
         const guess = userNumbers[i];
+        message.classList.remove("d-none");
+        instructions.classList.add("d-none");
         if (casualNumbersArray.includes(guess)) {
             guessedNumbers.push(guess);
-            instructions.innerHTML = `Hai indovinato ${guessedNumbers.length} numeri (${guessedNumbers})`;
+            message.innerHTML = `Hai indovinato ${guessedNumbers.length} numeri (${guessedNumbers})`;
         } else if (guessedNumbers.length === 0) {
-
-            instructions.innerHTML = `Non hai indovinato nessun numero!`;
+            message.innerHTML = `Non hai indovinato nessun numero!`;
         } else if (guessedNumbers.length === 1) {
-            instructions.innerHTML = `Hai indovinato ${guessedNumbers.length} numero (${guessedNumbers})`;
+            message.innerHTML = `Hai indovinato ${guessedNumbers.length} numero (${guessedNumbers})`;
         }
 
     }
+
     console.log(guessedNumbers);
 });
 
